45406c7a403d8e3c22222ff15ba5eaecfd12c3f9
howell <th15817161961@gmail.com>
2017-07-09 18:13:19 +0800

    v1.0.1

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,3 @@
-package run;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
